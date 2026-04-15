0995c7aeb9572d68450fe5032af2e90ba4ad1542
jaredrobg <jaredrobles1208@gmail.com>
2026-04-14 21:49:37 -0600

    fix: cambio de dominio API y config next

--- app/ProductosFiltrados/[filter]/page.tsx
+++ app/ProductosFiltrados/[filter]/page.tsx
@@ -3,6 +3,7 @@ import Productos from "../../Components/Productos";
 import { getProductos, ProductoFields } from '../../lib/contentful'; 
 import { Entry } from "contentful";
 
+export const dynamic = 'force-dynamic';
 
 export const revalidate = 60; // Revalidar cada 60 segundos
 
@@ -47,53 +48,7 @@ export default async function ProductosFiltrados({ params }: Props) {
   );
 }
 
-export async function generateStaticParams() {
-  return [
-    // Marcas
-    { filter: "al-haramain" },
-    { filter: "armani" },
-    { filter: "bharara" },
-    { filter: "cacharel" },
-    { filter: "calvin-klein" },
-    { filter: "carolina-herrera" },
-    { filter: "dolce-gabbana" },
-    { filter: "ferrari" },
-    { filter: "givenchy" },
-    { filter: "hugo-boss" },
-    { filter: "lacoste" },
-    { filter: "lancome" },
-    { filter: "lattafa" },
-    { filter: "love-moschino" },
-    { filter: "michel-kors" },
-    { filter: "montblanc" },
-    { filter: "olive-people" },
-    { filter: "paco-rabanne" },
-    { filter: "paris-hilton" },
-    { filter: "polo" },
-    { filter: "ralph-lauren" },
-    { filter: "rasasi" },
-    { filter: "rayban" },
-    { filter: "versace" },
-    { filter: "victorias-secret" },
-    { filter: "yves-saint-laurent" },
 
-    // Temporadas
-    { filter: "primavera" },
-    { filter: "verano" },
-    { filter: "otono" },
-    { filter: "invierno" },
-
-    // Categorías
-    { filter: "lente-solar" },
-    { filter: "lente-oftalmico" },
-    { filter: "fragancia" },
-    { filter: "fragancia-arabe" },
-    { filter: "accesorio" },
-
-    // Opción general
-    { filter: "todos" },
-  ];
-}
 
 export async function generateMetadata({ params }: Props) {
   const { filter } = await params;

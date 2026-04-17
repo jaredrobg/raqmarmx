6e8fbabe1800c0426b856a2d13df68de17339f4a
jaredrobg <jaredrobles1208@gmail.com>
2026-04-16 19:21:04 -0600

    quitar aviso de mantenimiento

--- app/Components/AvisoMantenimiento.tsx
+++ app/Components/AvisoMantenimiento.tsx
@@ -14,7 +14,7 @@ export default function AvisoMantenimiento(){
     }
 
     return(
-        <div className="avisoMantenimiento liquid-glass-red">
+        <div className="avisoMantenimiento liquid-glass-red" style={{display:"none"}}>
             <ImCross style={styles.icon} size={11} onClick={closeAviso}/>
             ¡AVISO! Esta pagina actualmente se encuentra en mantenimiento. Los precios e informacion de productos pueden no ser correctos.
         </div>

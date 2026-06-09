9709a896c445c0619b7061514b028be630acfd68
Desarrollo <desarrollo@192.168.1.5>
2026-06-09 14:40:49 -0600

    Implementacion Pixel de Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import SideMenu from "./Components/SideMenu";
 import { ShoppingBagModule } from "./ShoppingBag/page";
 import { Toaster } from 'react-hot-toast';
 import SearchButton from './Components/SearchButton';
+import Script from 'next/script';
 
 
 const geistSans = Geist({
@@ -42,6 +43,27 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet"></link>
+        {/* Meta Pixel Code */}
+       <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '3157761421086989');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript><img height="1" width="1" style={{ display: "none" }}
+        src="https://www.facebook.com/tr?id=3157761421086989&ev=PageView&noscript=1"
+        /></noscript>
+{/* <!-- End Meta Pixel Code --> */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <DisableConsole />
